9f5b8d645b4f3f418656a8fe69682a5944c0fd60
Lukasz W <lukasz.wiszniewski007@gmail.com>
2016-09-24 14:50:07 +0200

    ActionActivity  -new layouts  -fragmentmanager

--- app/src/main/java/pl/lukaszwiszniewski/hotchpotch/ActionActivity.java
+++ app/src/main/java/pl/lukaszwiszniewski/hotchpotch/ActionActivity.java
@@ -1,5 +1,7 @@
 package pl.lukaszwiszniewski.hotchpotch;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class ActionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
+
+        FragmentManager fragmentManager = getFragmentManager();
+
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.frameLayout,new FragmentWebView());
+        ft.commit();
     }
 }
